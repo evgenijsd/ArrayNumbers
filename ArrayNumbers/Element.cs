6e3204328420d6fdbafe999c5a59fc41f34e0fbd
yevhensd <yevhen.sd@gmail.com>
2022-10-22 15:55:34 +0300

    complete

--- ArrayNumbers/Element.cs
+++ ArrayNumbers/Element.cs
@@ -11,7 +11,7 @@ namespace ArrayNumbers
         public int BiasRow { get; set; }
         public int BiasColumn { get; set; }
 
-        public Element(int row, int column, int biasRow, int biasColumn)
+        public Element(int row, int column, int biasRow = 0, int biasColumn = 0)
         {
             Row = row;
             Column = column;
--- ArrayNumbers/Program.cs
+++ ArrayNumbers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ArrayNumbers
@@ -11,16 +12,25 @@ namespace ArrayNumbers
 
         static bool CheckingLine(int value, int count, Element element, ArrayNumbers datas, ArrayСonditions conditions)
         {
-            var nextElement = new Element(element.Row + element.BiasRow, element.Column + element.BiasColumn, element.BiasRow, element.BiasColumn);
-            bool check = conditions.DataCondition[element.Row, element.Column];
+            var nextElement = new Element(
+                element.Row + element.BiasRow, 
+                element.Column + element.BiasColumn, 
+                element.BiasRow, 
+                element.BiasColumn
+            );
+            bool check = false;            
 
             if (count > 2) check = true;
 
-            if (nextElement.Row < max_row &&  nextElement.Column < max_column && nextElement.Row > -1 && nextElement.Column > -1 && !check)
+            if (nextElement.Row < max_row &&  
+                nextElement.Column < max_column && 
+                nextElement.Row > -1 && 
+                nextElement.Column > -1)
             {
                 int nextValue = datas.DataArray[nextElement.Row, nextElement.Column];
+                bool checkNext = conditions.DataCondition[nextElement.Row, nextElement.Column];
 
-                if (nextValue == value)
+                if (nextValue == value && !checkNext)
                 {
                     count++;
                     check = CheckingLine(value, count, nextElement, datas, conditions);                    
@@ -31,56 +41,98 @@ namespace ArrayNumbers
             return check;
         }
 
+        static void MoveElement(Element element, ArrayNumbers datas, ArrayСonditions conditions)
+        {
+            var nextElement = new Element(
+                element.Row + element.BiasRow, 
+                element.Column + element.BiasColumn, 
+                element.BiasRow, 
+                element.BiasColumn
+            );
+
+            if (nextElement.Row < max_row && 
+                nextElement.Column < max_column && 
+                nextElement.Row > -1 && 
+                nextElement.Column > -1)
+            {
+                if (!conditions.DataCondition[nextElement.Row, nextElement.Column])
+                {
+                    int value = datas.DataArray[element.Row, element.Column];
+                    conditions.DataCondition[element.Row, element.Column] = false;
+                    datas.DataArray[element.Row, element.Column] = datas.DataArray[nextElement.Row, nextElement.Column];
+                    element.Row = nextElement.Row;
+                    element.Column = nextElement.Column;
+                    conditions.DataCondition[element.Row, element.Column] = true;                                        
+                    datas.DataArray[element.Row, element.Column] = value;
+                    MoveElement(element, datas, conditions);
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            int[,] array = {
-                    { 3, 3, 0, 2, 0, 3, 1, 3, 1 },
-                    { 3, 0, 1, 3, 2, 2, 2, 1, 2 },
-                    { 3, 1, 0, 2, 2, 3, 2, 3, 3 },
-                    { 0, 1, 2, 2, 1, 3, 3, 1, 0 },
-                    { 0, 2, 1, 1, 0, 2, 3, 1, 3 },
-                    { 0, 3, 1, 3, 1, 0, 1, 1, 0 },
-                    { 0, 3, 1, 0, 0, 1, 0, 1, 2 },
-                    { 3, 1, 2, 2, 3, 1, 1, 2, 2 },
-                    { 1, 3, 1, 1, 2, 0, 3, 2, 2 }
-                  };
             var datas = new ArrayNumbers(max_row, max_column);
             var conditions = new ArrayСonditions(max_row, max_column);
-            datas.DataArray = array;
+            var random = new Random();
+            var listConditions = new List<Element>();
+
+            do
+            {
+                listConditions.Clear();
+
+                for (int i = 0; i < max_row; i++)
+                {
+                    for (int j = 0; j < max_column; j++)
+                    {
+                        if (!conditions.DataCondition[i, j])
+                        {
+                            var nextElement = new Element(i, j, 0, 1);
+                            var value = datas.DataArray[i, j];
+
+                            conditions.DataCondition[i, j] = CheckingLine(value, 1, nextElement, datas, conditions);
+
+                            if (!conditions.DataCondition[i, j])
+                            {
+                                nextElement = new Element(i, j, 1, 0);
+                                conditions.DataCondition[i, j] = CheckingLine(value, 1, nextElement, datas, conditions);
+                            }
+                        }
+
+                        if (conditions.DataCondition[i, j]) listConditions.Add(new Element(i, j, -1, 0));
+                    }
+                }
+
+                foreach (var element in listConditions)
+                {
+                    MoveElement(element, datas, conditions);
+                }
+
+                foreach (var element in listConditions)
+                {
+                    conditions.DataCondition[element.Row, element.Column] = false;
+                    datas.DataArray[element.Row, element.Column] = random.Next(0, 4);
+                }
+            }
+            while (listConditions.Count > 0);
+
+            Console.WriteLine();
 
             for (int i = 0; i < max_row; i++)
             {
                 for (int j = 0; j < max_column; j++)
-                {                    
-                    if (!conditions.DataCondition[i,j])
-                    {
-                        var nextElement = new Element(i, j, 0, 1);
-                        var value = datas.DataArray[i, j];
-
-                        conditions.DataCondition[i, j] = CheckingLine(value, 1, nextElement, datas, conditions);
-
-                        if (!conditions.DataCondition[i, j])
-                        {
-                            nextElement = new Element(i, j, 1, 0);
-                            conditions.DataCondition[i, j] = CheckingLine(value, 1, nextElement, datas, conditions);
-                        }
-                    }
+                {
+                    Console.Write($" {datas.DataArray[i, j]}");
                 }
+                Console.WriteLine();
             }
 
-            using (StreamWriter file = File.CreateText("array_numbers.json"))
+            using (StreamWriter file = File.CreateText("array_numbers_log.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 serializer.Serialize(file, datas);
-                Console.WriteLine("Data has been saved to file");
-            }
-
-            using (StreamWriter file = File.CreateText("array_conditions.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, conditions);
-                Console.WriteLine("Data has been saved to file");
+                Console.WriteLine();
+                Console.WriteLine(" Data has been saved to file. Press any key...");
+                Console.ReadKey();
             }
         }
     }
